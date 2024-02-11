cb14b0781879ccdb26f4e022ca3c20a415c3233e
olhaHrb <150930419+olhaHrb@users.noreply.github.com>
2024-02-11 20:49:37 +0200

    hw3

--- js/task-4.js
+++ js/task-4.js
@@ -1,24 +1,26 @@
 'use strict';
-
+let message = "";
 function getShippingCost(country) {
     switch (country) {
         case "China":
-            return("Shipping to China will cost 100 credits");
+            message = "Shipping to China will cost 100 credits";
             break;
         case "Chile":
-            return("Shipping to Chile will cost 250 credits");
+            message = "Shipping to Chile will cost 250 credits";
             break;
         case "Australia":
-            return("Shipping to Australia will cost 170 credits");
+            message = "Shipping to Australia will cost 170 credits";
             break;
         case "Jamaica":
-            return("Shipping to Jamaica will cost 120 credits");
+            message = "Shipping to Jamaica will cost 120 credits";
             break;
         default:
-            return("Sorry, there is no delivery to your country");
+            message = "Sorry, there is no delivery to your country";
             break;
     }
 }
+
+return message;
 
 
 
