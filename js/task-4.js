5afd865147fd66bfab67de62016d239c7eb74abb
olhaHrb <150930419+olhaHrb@users.noreply.github.com>
2024-02-11 14:05:51 +0200

    hw2

--- js/task-4.js
+++ js/task-4.js
@@ -3,27 +3,22 @@
 function getShippingCost(country) {
     switch (country) {
         case "China":
-            console.log("Shipping to China will cost 100 credits");
+            return("Shipping to China will cost 100 credits");
             break;
         case "Chile":
-            console.log("Shipping to Chile will cost 250 credits");
+            return("Shipping to Chile will cost 250 credits");
             break;
         case "Australia":
-            console.log("Shipping to Australia will cost 170 credits");
+            return("Shipping to Australia will cost 170 credits");
             break;
         case "Jamaica":
-            console.log("Shipping to Jamaica will cost 120 credits");
+            return("Shipping to Jamaica will cost 120 credits");
             break;
         default:
-            console.log("Sorry, there is no delivery to your country");
+            return("Sorry, there is no delivery to your country");
             break;
     }
 }
 
 
-getShippingCost("Australia");
-getShippingCost("Germany");
-getShippingCost("China");
-getShippingCost("Chile");
-getShippingCost("Jamaica");
-getShippingCost("Sweden");
+
